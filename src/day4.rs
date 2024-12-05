ec4f39f3e1e5d34767b773a268efa00b29b5cfb6
unvariant <unvariant.winter@gmail.com>
2024-12-05 03:08:38 -0800

    pretty sure this is the same thing

--- src/day4.rs
+++ src/day4.rs
@@ -200,7 +200,7 @@ pub fn part1(s: &str) -> impl std::fmt::Display {
 unsafe fn cross(s: &[u8]) -> u32 {
     let mut ptr = s.as_ptr();
     // stop 2 rows before since we read at current row to current row + 2
-    let end = s.as_ptr().add(141 * 140).sub(141 * 2);
+    let end = s.as_ptr().add(128 * 151 + 1);
 
     macro_rules! index {
         ($inc:expr, $row:expr, $off:expr) => {
