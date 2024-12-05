fb70cae8882fa13f70767d71af3b6a5555a98535
unvariant <unvariant.winter@gmail.com>
2024-12-04 18:02:48 -0800

    actually revert

--- src/day4.rs
+++ src/day4.rs
@@ -1,18 +1,81 @@
 use super::*;
 
-#[derive(Clone, Copy)]
-#[repr(align(64))]
-struct Row {
-    x: Bits,
-    m: Bits,
-    a: Bits,
-    s: Bits,
+struct Row32 {
+    x: u128,
+    m: u128,
+    a: u128,
+    s: u128,
 }
 
-#[derive(Clone, Copy)]
-struct Bits {
-    a: u128,
-    b: u64,
+macro_rules! row32 {
+    ($expression:expr, $x:expr, $m:expr, $a:expr, $s:expr) => {{
+        let curr = $expression as *const u8x32;
+
+        let r0 = curr.add(0).read_unaligned();
+        let r1 = curr.add(1).read_unaligned();
+        let r2 = curr.add(2).read_unaligned();
+        let r3 = curr.add(3).read_unaligned();
+
+        Row32 {
+            x: (r0.simd_eq($x).to_bitmask() as u128
+                | ((r1.simd_eq($x).to_bitmask() as u128) << 32)
+                | ((r2.simd_eq($x).to_bitmask() as u128) << 64)
+                | ((r3.simd_eq($x).to_bitmask() as u128) << 96)),
+            m: r0.simd_eq($m).to_bitmask() as u128
+                | ((r1.simd_eq($m).to_bitmask() as u128) << 32)
+                | ((r2.simd_eq($m).to_bitmask() as u128) << 64)
+                | ((r3.simd_eq($m).to_bitmask() as u128) << 96),
+            a: r0.simd_eq($a).to_bitmask() as u128
+                | ((r1.simd_eq($a).to_bitmask() as u128) << 32)
+                | ((r2.simd_eq($a).to_bitmask() as u128) << 64)
+                | ((r3.simd_eq($a).to_bitmask() as u128) << 96),
+            s: r0.simd_eq($s).to_bitmask() as u128
+                | ((r1.simd_eq($s).to_bitmask() as u128) << 32)
+                | ((r2.simd_eq($s).to_bitmask() as u128) << 64)
+                | ((r3.simd_eq($s).to_bitmask() as u128) << 96),
+        }
+    }};
+}
+
+struct Row16 {
+    x: u64,
+    m: u64,
+    a: u64,
+    s: u64,
+}
+
+#[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
+unsafe fn row16(base: *const u8) -> Row16 {
+    let x = u8x16::splat(b'X');
+    let m = u8x16::splat(b'M');
+    let a = u8x16::splat(b'A');
+    let s = u8x16::splat(b'S');
+
+    let curr = base as *const u8x16;
+
+    let r0 = curr.add(0).read_unaligned();
+
+    Row16 {
+        x: r0.simd_eq(x).to_bitmask(),
+        m: r0.simd_eq(m).to_bitmask(),
+        a: r0.simd_eq(a).to_bitmask(),
+        s: r0.simd_eq(s).to_bitmask(),
+    }
+}
+
+macro_rules! row16 {
+    ($expression:expr, $x:expr, $m:expr, $a:expr, $s:expr) => {{
+        let curr = $expression as *const u8x16;
+
+        let r0 = curr.add(0).read_unaligned();
+
+        Row16 {
+            x: r0.simd_eq($x).to_bitmask(),
+            m: r0.simd_eq($m).to_bitmask(),
+            a: r0.simd_eq($a).to_bitmask(),
+            s: r0.simd_eq($s).to_bitmask(),
+        }
+    }};
 }
 
 #[export_name = "part1"]
@@ -31,66 +94,40 @@ unsafe fn scan(s: &[u8]) -> u32 {
     let a16 = u8x16::splat(b'A');
     let s16 = u8x16::splat(b'S');
 
-    let mut rows: [Row; 143] = [Row {
-        x: Bits { a: 0, b: 0 },
-        m: Bits { a: 0, b: 0 },
-        a: Bits { a: 0, b: 0 },
-        s: Bits { a: 0, b: 0 },
-    }; 143];
+    let mut row0x32;
+    let mut row1x32 = row32!(ptr.add(141 * 0), x32, m32, a32, s32);
+    let mut row2x32 = row32!(ptr.add(141 * 1), x32, m32, a32, s32);
+    let mut row3x32 = row32!(ptr.add(141 * 2), x32, m32, a32, s32);
 
-    for i in 0..140 {
-        let base = ptr as *const u8x32;
-        let a = base.add(0).read_unaligned();
-        let b = base.add(1).read_unaligned();
-        let c = base.add(2).read_unaligned();
-        let d = base.add(3).read_unaligned();
-        let e = (ptr.add(125) as *const u8x16).read_unaligned();
+    let mut row0x16;
+    let mut row1x16 = row16!(ptr.add(141 * 0 + 125), x16, m16, a16, s16);
+    let mut row2x16 = row16!(ptr.add(141 * 1 + 125), x16, m16, a16, s16);
+    let mut row3x16 = row16!(ptr.add(141 * 2 + 125), x16, m16, a16, s16);
 
-        rows[i].x.a = (a.simd_eq(x32).to_bitmask() as u128)
-            | ((b.simd_eq(x32).to_bitmask() as u128) << 32)
-            | ((c.simd_eq(x32).to_bitmask() as u128) << 64)
-            | ((d.simd_eq(x32).to_bitmask() as u128) << 96);
-        rows[i].x.b = e.simd_eq(x16).to_bitmask();
+    for _ in 0..140 {
+        let next = ptr.add(141);
+        row0x32 = row1x32;
+        row1x32 = row2x32;
+        row2x32 = row3x32;
+        row3x32 = row32!(ptr.add(141 * 3), x32, m32, a32, s32);
 
-        rows[i].m.a = (a.simd_eq(m32).to_bitmask() as u128)
-            | ((b.simd_eq(m32).to_bitmask() as u128) << 32)
-            | ((c.simd_eq(m32).to_bitmask() as u128) << 64)
-            | ((d.simd_eq(m32).to_bitmask() as u128) << 96);
-        rows[i].m.b = e.simd_eq(m16).to_bitmask();
+        row0x16 = row1x16;
+        row1x16 = row2x16;
+        row2x16 = row3x16;
+        row3x16 = row16!(ptr.add(141 * 3 + 125), x16, m16, a16, s16);
 
-        rows[i].a.a = (a.simd_eq(a32).to_bitmask() as u128)
-            | ((b.simd_eq(a32).to_bitmask() as u128) << 32)
-            | ((c.simd_eq(a32).to_bitmask() as u128) << 64)
-            | ((d.simd_eq(a32).to_bitmask() as u128) << 96);
-        rows[i].a.b = e.simd_eq(a16).to_bitmask();
-
-        rows[i].s.a = (a.simd_eq(s32).to_bitmask() as u128)
-            | ((b.simd_eq(s32).to_bitmask() as u128) << 32)
-            | ((c.simd_eq(s32).to_bitmask() as u128) << 64)
-            | ((d.simd_eq(s32).to_bitmask() as u128) << 96);
-        rows[i].s.b = e.simd_eq(s16).to_bitmask();
-
-        ptr = ptr.add(141);
-    }
-
-    for i in 0..140 {
-        let horizontal_forward =
-            rows[i].x.a & (rows[i].m.a << 1) & (rows[i].a.a << 2) & (rows[i].s.a << 3);
+        let horizontal_forward = row0x32.x & (row0x32.m << 1) & (row0x32.a << 2) & (row0x32.s << 3);
         let horizontal_backward =
-            rows[i].s.a & (rows[i].a.a << 1) & (rows[i].m.a << 2) & (rows[i].x.a << 3);
+            row0x32.s & (row0x32.a << 1) & (row0x32.m << 2) & (row0x32.x << 3);
 
-        let vertical_forward = rows[i].x.a & rows[i + 1].m.a & rows[i + 2].a.a & rows[i + 3].s.a;
-        let vertical_backward = rows[i].s.a & rows[i + 1].a.a & rows[i + 2].m.a & rows[i + 3].x.a;
+        let vertical_forward = row0x32.x & row1x32.m & row2x32.a & row3x32.s;
+        let vertical_backward = row0x32.s & row1x32.a & row2x32.m & row3x32.x;
 
-        let top_left_forward =
-            rows[i].x.a & (rows[i + 1].m.a >> 1) & (rows[i + 2].a.a >> 2) & (rows[i + 3].s.a >> 3);
-        let top_left_backward =
-            rows[i].s.a & (rows[i + 1].a.a >> 1) & (rows[i + 2].m.a >> 2) & (rows[i + 3].x.a >> 3);
+        let top_left_forward = row0x32.x & (row1x32.m >> 1) & (row2x32.a >> 2) & (row3x32.s >> 3);
+        let top_left_backward = row0x32.s & (row1x32.a >> 1) & (row2x32.m >> 2) & (row3x32.x >> 3);
 
-        let top_right_forward =
-            rows[i].x.a & (rows[i + 1].m.a << 1) & (rows[i + 2].a.a << 2) & (rows[i + 3].s.a << 3);
-        let top_right_backward =
-            rows[i].s.a & (rows[i + 1].a.a << 1) & (rows[i + 2].m.a << 2) & (rows[i + 3].x.a << 3);
+        let top_right_forward = row0x32.x & (row1x32.m << 1) & (row2x32.a << 2) & (row3x32.s << 3);
+        let top_right_backward = row0x32.s & (row1x32.a << 1) & (row2x32.m << 2) & (row3x32.x << 3);
 
         total += horizontal_forward.count_ones()
             + horizontal_backward.count_ones()
@@ -104,23 +141,18 @@ unsafe fn scan(s: &[u8]) -> u32 {
         const MASK: u64 = 0xffffffffffffffffu64 & !0b111u64;
 
         let horizontal_forward =
-            rows[i].x.b & (rows[i].m.b << 1) & (rows[i].a.b << 2) & (rows[i].s.b << 3) & MASK;
+            row0x16.x & (row0x16.m << 1) & (row0x16.a << 2) & (row0x16.s << 3) & MASK;
         let horizontal_backward =
-            rows[i].s.b & (rows[i].a.b << 1) & (rows[i].m.b << 2) & (rows[i].x.b << 3) & MASK;
+            row0x16.s & (row0x16.a << 1) & (row0x16.m << 2) & (row0x16.x << 3) & MASK;
 
-        let vertical_forward =
-            rows[i].x.b & rows[i + 1].m.b & rows[i + 2].a.b & rows[i + 3].s.b & MASK;
-        let vertical_backward =
-            rows[i].s.b & rows[i + 1].a.b & rows[i + 2].m.b & rows[i + 3].x.b & MASK;
+        let vertical_forward = row0x16.x & row1x16.m & row2x16.a & row3x16.s & MASK;
+        let vertical_backward = row0x16.s & row1x16.a & row2x16.m & row3x16.x & MASK;
 
-        let top_left_forward =
-            rows[i].x.b & (rows[i + 1].m.b >> 1) & (rows[i + 2].a.b >> 2) & (rows[i + 3].s.b >> 3);
-        let top_left_backward =
-            rows[i].s.b & (rows[i + 1].a.b >> 1) & (rows[i + 2].m.b >> 2) & (rows[i + 3].x.b >> 3);
-        let top_right_forward =
-            rows[i].x.b & (rows[i + 1].m.b << 1) & (rows[i + 2].a.b << 2) & (rows[i + 3].s.b << 3);
-        let top_right_backward =
-            rows[i].s.b & (rows[i + 1].a.b << 1) & (rows[i + 2].m.b << 2) & (rows[i + 3].x.b << 3);
+        let top_left_forward = row0x16.x & (row1x16.m >> 1) & (row2x16.a >> 2) & (row3x16.s >> 3);
+        let top_left_backward = row0x16.s & (row1x16.a >> 1) & (row2x16.m >> 2) & (row3x16.x >> 3);
+
+        let top_right_forward = row0x16.x & (row1x16.m << 1) & (row2x16.a << 2) & (row3x16.s << 3);
+        let top_right_backward = row0x16.s & (row1x16.a << 1) & (row2x16.m << 2) & (row3x16.x << 3);
 
         total += horizontal_forward.count_ones()
             + horizontal_backward.count_ones()
@@ -130,6 +162,7 @@ unsafe fn scan(s: &[u8]) -> u32 {
             + top_left_backward.count_ones()
             + top_right_forward.count_ones()
             + top_right_backward.count_ones();
+        ptr = next;
     }
 
     total
